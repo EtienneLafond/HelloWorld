62e11b03d3955e2723c3ee2c597ed7f58a101b95
EtienneLafond <e.tiennel@hotmail.com>
2022-06-06 20:49:33 -0300

    Hello World First commit
    Signed-off-by: EtienneLafond <e.tiennel@hotmail.com>

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * HelloWorld simply displays "Hello World!" to the standard display
+ *
+ * @author  Etienne Lafond
+ * @version 1.0
+ * @since   2022-06-06
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints Hello, World! on standard output.
+        System.out.println("Hello World!");
+    }
+}
